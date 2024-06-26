222da1024b5e0e11e8aed078271d684c1f8fef44
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2024-06-26 15:59:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,8 +52,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://acefrom52:09956243658@cluster0.odztfai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "tjbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tjbotdatabase')
+DATABASE_NAME = environ.get('DATABASE_NAME', "tj")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tjbot')
 
 # Premium And Referal Settings
 PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) # Set Ture Or False
