0637348c07ccfbbddc8a0fd7f5087680a2e6f559
TJBOTS <teekam4334@gmail.com>
2025-01-02 16:30:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Teekam:9571404334@cluster0.pokkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Teekam:9571404334@cluster0.pokkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
